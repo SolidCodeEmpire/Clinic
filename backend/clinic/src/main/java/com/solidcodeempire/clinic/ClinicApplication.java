f5ac531cd133cdb692f8a95ebe7c8d5cd6645a58
Michał Pilch <m.pilch2002@gmail.com>
2024-03-22 12:33:47 +0100

    0.1 - Created project files

---
+++ backend/clinic/src/main/java/com/solidcodeempire/clinic/ClinicApplication.java
@@ -0,0 +1,13 @@
+package com.solidcodeempire.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicApplication.class, args);
+	}
+
+}
---
+++ backend/clinic/src/test/java/com/solidcodeempire/clinic/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidcodeempire.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
