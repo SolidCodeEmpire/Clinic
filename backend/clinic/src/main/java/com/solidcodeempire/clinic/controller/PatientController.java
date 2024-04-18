8d66f7eec488657d49534d881b40a1b36c5af350
Michał Pilch <m.pilch2002@gmail.com>
2024-04-18 17:31:42 +0200

    0.2.1 - Added springdoc (Swagger support)

--- backend/clinic/src/main/java/com/solidcodeempire/clinic/controller/PatientController.java
+++ backend/clinic/src/main/java/com/solidcodeempire/clinic/controller/PatientController.java
@@ -1,15 +1,19 @@
 package com.solidcodeempire.clinic.controller;
 
 import com.solidcodeempire.clinic.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name="Patient")
 public class PatientController {
     final private PatientService patientService;
 
     @GetMapping("/patient/{id}")
+    @Operation(summary="Get patient specified by ID")
     public String getPatient(@PathVariable int id) {
 
         return patientService.getPatient(id);
