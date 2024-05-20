8d44fde6caaafefe9fccbced808476e4600bf845
Dave <dj300758@student.polsl.pl>
2024-05-20 16:54:23 +0200

    0.3.1 added patient validation

--- frontend/src/Components/Contents/AddPatient/AddPatient.tsx
+++ frontend/src/Components/Contents/AddPatient/AddPatient.tsx
@@ -7,8 +7,29 @@ import { Patient } from '../../../API/Model/PatientModel';
 export default function AddPatient() {
   const [patient, setPatient] = useState<Patient>()
 
+  /**
+   * Check if non-null values in database are defined 
+   * and assure that given ssn, insurence and phone number are correct (number of digits).
+   * 
+   * @param patient 
+   * @returns 
+   */
   function validatePatient(patient: Patient) {
-    return true;
+    if(+patient.socialSecurityNumber < 100000000 || +patient.socialSecurityNumber > 999999999 || patient.insuranceNumber === undefined
+        || +patient.insuranceNumber < 100000000  || +patient.insuranceNumber > 999999999      || patient.insuranceNumber === undefined
+        || +patient.phoneNumber < 100000000      || +patient.phoneNumber > 999999999          || patient.phoneNumber === undefined
+        || (patient.firstName
+        && patient.lastName 
+        && patient.sex 
+        && patient.dateOfBirth  
+        && patient.address.street 
+        && patient.address.houseNumber  
+        && patient.address.city  
+        && patient.address.postalCode  
+        && patient.address.country)) 
+      return false;
+    else 
+      return true;
   }
 
   return (
@@ -17,9 +38,18 @@ export default function AddPatient() {
         <h1 className="add-patient-header">Insert patient Data</h1>
         <PatientForm patient={patient} disabled={false} patientDispatcher={setPatient}></PatientForm>
         <div className='add-patient-button-div'>
-          <button className='add-patient-button' onClick={()=> patient && validatePatient(patient) && submitPatient(patient).then(()=>{
-            setPatient(undefined)
-          })}>Submit Data</button>
+          <button className='add-patient-button' onClick={()=>{
+            if(window.confirm("Are you sure you want to add new patient?")){
+              if(patient && validatePatient(patient)){
+                submitPatient(patient)
+                setPatient(undefined)
+                alert("New patient added successfully.")
+              } 
+              else{
+                alert("Adding new patient failed. Check input data.")
+              }
+            }
+          }}>Submit Data</button>
         </div>
       </div>
     </>
