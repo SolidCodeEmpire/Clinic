fca18dd544a128a7b9e2e0ff24b438c4949c56f6
kamilkubina <kamil1202@interia.pl>
2024-05-21 13:08:32 +0200

    0.3.2 fixed findPatient

--- backend/clinic/src/main/java/com/solidcodeempire/clinic/repository/PatientRepository.java
+++ backend/clinic/src/main/java/com/solidcodeempire/clinic/repository/PatientRepository.java
@@ -15,9 +15,9 @@ import java.util.Optional;
 public interface PatientRepository extends CrudRepository<Patient, Long> {
     Optional<Patient> findById(long ID);
 
-    @Query("SELECT p FROM Patient p join fetch p.address WHERE (:firstName IS NULL OR p.firstName = :firstName) " +
-            "AND (:lastName IS NULL OR p.lastName = :lastName) " +
-            "AND (:socialSecurityNumber IS NULL OR p.socialSecurityNumber = :socialSecurityNumber)")
+    @Query("SELECT p FROM Patient p join fetch p.address WHERE (:firstName IS NULL OR p.firstName ILIKE %:firstName%) " +
+            "AND (:lastName IS NULL OR p.lastName ILIKE %:lastName%) " +
+            "AND (:socialSecurityNumber IS NULL OR CAST(p.socialSecurityNumber AS string) LIKE %:socialSecurityNumber%)")
     List<Patient> findPatient(String firstName, String lastName, Integer socialSecurityNumber);
 
     @Query("select p from Patient p join fetch p.address")
