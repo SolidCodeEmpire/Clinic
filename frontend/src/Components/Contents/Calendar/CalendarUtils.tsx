0bb16f3fbd02346541c40d8e81d4dab3ebca69c3
Dave <dj300758@student.polsl.pl>
2024-06-22 19:05:54 +0200

    0.5.4 date bug fixed

--- frontend/src/Components/Contents/Calendar/CalendarUtils.tsx
+++ frontend/src/Components/Contents/Calendar/CalendarUtils.tsx
@@ -3,20 +3,23 @@ import React from "react";
 export function getStartOfWeek(date: Date) {
   const startDate = new Date(date);
   const day = startDate.getDay();
-  const diff = startDate.getDate() - day + 2; // Adjust when day is Sunday
+  const diff = startDate.getDate() - day + (day === 0 ? -6 : 1); // Adjust when day is Sunday
   startDate.setDate(diff);
   return startDate;
 }
+
 export function startOfWeek(currentDate: Date) {
   const startDate = new Date(currentDate);
   startDate.setDate(startDate.getDate() - startDate.getDay() + 2);
   return startDate;
 }
+
 export function endOfWeek(currentDate: Date) {
   const endDate = new Date(currentDate);
   endDate.setDate(endDate.getDate() + 7 - endDate.getDay() - 1);
   return endDate;
 }
+
 export function mapTimeToIndexedValues(
   setDayToDate: React.Dispatch<React.SetStateAction<string[]>>,
   startDate: Date,
